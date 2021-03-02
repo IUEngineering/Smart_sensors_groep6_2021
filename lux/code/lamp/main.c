a84bd392c01f657b31db863d0278c370ed31bc2d
NielsZw <66211233+NielsZw@users.noreply.github.com>
2021-03-02 17:46:42 +0100

    Lux update
    C code LUX done + comments

--- lux/code/lamp/main.c
+++ lux/code/lamp/main.c
@@ -6,33 +6,28 @@
 #include "clock.h"
 #include "serialF0.h"
 
-#define MAX_VALUE   2047                                   // only 11 bits are used
-#define VCC         3.30
-#define VREF        (((double) VCC) / 1.6)
 
-void init_adc(void)
+void init_adc(void) //Initializing ADC
 {
-	PORTA.DIRCLR     = PIN2_bm;                      
-	ADCA.CH0.MUXCTRL = ADC_CH_MUXPOS_PIN2_gc | ADC_CH_MUXNEG_GND_MODE3_gc;       
-	ADCA.CH0.CTRL    = ADC_CH_INPUTMODE_SINGLEENDED_gc;           
-	ADCA.REFCTRL     = ADC_REFSEL_INTVCC_gc;
-	ADCA.CTRLB       = ADC_RESOLUTION_12BIT_gc | ADC_CONMODE_bm;                  
-	ADCA.PRESCALER   = ADC_PRESCALER_DIV32_gc;
+	PORTA.DIRCLR     = PIN3_bm;                                                 // PA3 = ADC input
+	ADCA.CH0.MUXCTRL = ADC_CH_MUXPOS_PIN3_gc | ADC_CH_MUXNEG_GND_MODE4_gc;      // Set PIN3 as positive | set GND als negative
+	ADCA.CH0.CTRL    = ADC_CH_INPUTMODE_DIFFWGAIN_gc | ADC_CH_GAIN_DIV2_gc;     // GAIN * 1/2
+	ADCA.REFCTRL     = ADC_REFSEL_INTVCC_gc;                                    // VCC/1.6 (VCC=3.3V)
+	ADCA.CTRLB       = ADC_RESOLUTION_12BIT_gc | ADC_CONMODE_bm;                                
+	ADCA.PRESCALER   = ADC_PRESCALER_DIV128_gc;									// 32M/128 is 250.000Hz ADC clock
 	ADCA.CTRLA       = ADC_ENABLE_bm;
 }
 
-void init_pwm(void)
+void init_pwm(void) //Initializing PWM 
 {
-	PORTE.DIRSET = PIN0_bm;
-
-	
-	TCE0.CTRLB   = TC0_CCAEN_bm | TC_WGMODE_SINGLESLOPE_gc;
-	TCE0.CTRLA   = TC_CLKSEL_DIV8_gc;
-	TCE0.PER     = 9999;
-	TCE0.CCA     = 0;
+	PORTE.DIRSET = PIN0_bm;                                     // PD8 = PWM output       
+	TCE0.CTRLB   = TC0_CCAEN_bm | TC_WGMODE_SINGLESLOPE_gc;     //Singelsope PWM signal
+	TCE0.CTRLA   = TC_CLKSEL_DIV8_gc;                           // 32M/8 = 400.000
+	TCE0.PER     = 9999;                                        // 400K/(9999+1) = 400Hz PWM freq
+	TCE0.CCA     = 0;                                           // Duty-cycle starts at 0 
 }
 
-int16_t read_adc(void)                                     
+int16_t read_adc(void) //Reading the ADC                                     
 {
 	int16_t res;                                             
 
@@ -44,11 +39,6 @@ int16_t read_adc(void)
 	return res;
 }
 
-ISR(PORTD_INT0_vect)
-{
-	PORTA.OUTTGL = PIN2_bm;
-}
-
 int main(void)
 {	                                        
 	init_stream(F_CPU);
@@ -58,26 +48,26 @@ int main(void)
 	init_adc();
 	init_pwm();
 	
-	int16_t res; 
-	double vinp;  
-
-	sei();
+	int16_t res;  
+	double pwm;
 
 	while (1) {
 		
-		res = read_adc();
-		vinp = (double) res * VREF / (MAX_VALUE + 1);    
-		if(bit_is_clear(PORTA.IN, 0)){
-			if (res < 0){
-				res = 0;
-			}
-			TCE0.CCABUF = (res*4.9);
-		}
+		res = read_adc();     //Max_value = 1680
+		pwm = res * 5.95;     //MAX_value * 5.95 = 9996 | Max_value = 99.9% duty-cycle PWM
 		
-				
- 
- 
-		printf("res: %d  spanning: %.2f V\n",  res, vinp);
+		if (res < 0){
+			res = 0;
+		}
+			if (pwm < 0){
+				pwm = 0;
+			} 
+			
+		if(bit_is_clear(PORTE.IN, 0)){
+		TCE0.CCABUF = (pwm);     //Duty-cycle control | if pwm=0 than duty-cycle=0%, if pwm=9999 than duty-cycle=100%
+		}
+		 
+		printf("res: %d  pwm: %.1f \n",  res, pwm);
 		_delay_ms(200);
 	}
 }
